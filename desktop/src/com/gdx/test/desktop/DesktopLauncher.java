ca8f98f3468e4a6490c9ac6d207976f6bb0229f2
nism0 <nismo.sdt@gmail.com>
2017-10-11 19:33:39 +0200

    init

---
+++ desktop/src/com/gdx/test/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gdx.test.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gdx.test.test;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new test(), config);
+	}
+}
